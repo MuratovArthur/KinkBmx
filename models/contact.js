17f93f134f6e488024a38234164cce21f492a064
MuratovArthur <arturlove45@gmail.com>
2020-07-15 04:20:33 +0600

    Create contact model

---
+++ models/contact.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+var ContactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+module.exports = mongoose.model("Contact", ContactSchema);
